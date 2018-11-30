9fed9992a0d94cfc22d47916895840b25a0c7c06
Jairo Caicedo <jairotaker@hotmail.es>
2018-11-30 01:15:59 -0500

    agregando buscador

---
+++ Js/lecturaArchivo.js
@@ -0,0 +1,98 @@
+function addNew(titulo, vendido, precio) {
+   var articulo = $("<article/>", {
+      "class":"col-12 col-12 col-sm-12"
+    });
+
+    var figure = $("<figure/>", {
+      "class":"row col-12 col-sm-12"
+    });
+
+    var img = $("<img/>",{
+      "class": "col-xl-4 img-fluid col-6 col-sm-2",
+      "src": "https://via.placeholder.com/100x100"
+    });
+
+    var header = $( "<header/>", {
+      "class":"col-8 col-xl-8"
+    });
+
+    var div = $( "<div/>", {
+      "class": "col-12 col-sm-12",
+      "id": "articulo"
+    });
+
+    var a = $( "<a/>", {
+      "href": "../inforaccesorio/compraAudifono.html"
+    });
+
+    var h3 = $( "<h3/>", {
+      html: titulo
+    });
+
+    var p = $( "<p/>", {
+      html: vendido + " vendidos - audifonos - Guayas - Guayaquil"
+    });
+
+    var p1 = $( "<p/>", {
+      html: "U$S " + precio
+    });
+
+    figure.appendTo(articulo)
+    img.appendTo(figure);
+    header.appendTo(figure);
+    div.appendTo(header);
+    a.appendTo(div);
+    h3.appendTo(a);
+    p.appendTo(div);
+    p1.appendTo(div);
+
+    articulo.appendTo( "#articulos" );
+}
+
+
+function loadNewsJson() {
+   $.getJSON( "../base_datos/inventario.json", function( data ) {
+    
+      $.each( data, function( key, val ) {
+        //addNew(val.titulo, val.descripcion)
+        $.each(val, function(key,val){
+          //addNew(valor.titulo, valor.descripcion)
+          $.each(val, function(key,valor){
+            addNew(valor.titulo, valor.vendidos, valor.Precio);
+            console.log(valor);
+          });
+        });
+      });
+    
+  });
+}
+
+$(document).ready(function(){
+  loadNewsJson();
+
+  $("button#buscar").click(function(e){
+
+    var texto = $('input#buscador').val();
+    
+    if(texto.length != 0) {
+      
+      var noticias = $('#articulo');
+      $('#articulo').filter(function(index){
+        
+        $(this).show();
+        
+        var noticia = $(this).text()
+        if(noticia.indexOf(texto) == -1) {
+          $(this).hide()
+        }
+
+      });
+
+    } else {
+      $('#articulo').each(function(){
+        $(this).show();
+      });
+    }
+    return false;
+  })
+});
