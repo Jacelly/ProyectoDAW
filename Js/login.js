29120823a16dc3f81b0669fcbbc807cfdb922f74
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-11-30 00:56:56 -0500

    yaaa

--- Js/login.js
+++ Js/login.js
@@ -12,8 +12,9 @@ $(document).ready(function(){
         var password = $('input#myInput').val();
         var error =false;
         //alert(username);
-        $.getJSON("../base_datos/user.json", function(result){
-                $.each(result, function(i, field){
+ 	$.getJSON("../DB/user.json", function(result){
+                data = $.parseJSON(data);
+		$.each(result, function(i, field){
                     alert(field.usuario);
                     if( username == field.usuario && password == field.password){
                         alert("Login success");
@@ -23,39 +24,6 @@ $(document).ready(function(){
                         alert("ops, error user or password");
                     }
                 });
-            });
-        /*$.getJSON("../base_datos/user.json", function( data ) {
-    
-            $.each( data, function( key, val ) {
-            //addNew(val.titulo, val.descripcion)
-                alert(val+" ";
-            if( username == val.usuario && password == value.password){
-                        alert("Login success");
-                        error=true;
-                    }
-                    if(error == false){
-                        alert("ops, error user or password");
-            }
-                });
-    
-            });*/
-        /*
-        alert(username);
-        $.ajax({type: "POST",
-                url: "base_datos/user.json",
-                dataType:"json",
-                success : function(data){
-                  $.each(data,funtion(key,value){
-                    if( username == value.usuario && password == value.password){
-                        alert("hey");
-                        error=true;
-                        return false;
-                    }
-                    if(error == false){
-                        alert("ops");
-                        return false;
-                    }  
-                });
-        }});*/
+            });       
     });
 });
